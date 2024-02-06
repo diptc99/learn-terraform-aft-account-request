925afbee6fef6c049b9a50567f43ee01f82f2a43
diptc99 <diptangshu.chak@hcl.com>
2024-02-06 12:02:35 +0530

    New account requested

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,57 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+module "sandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "AWS_SECURITY@hcl.com"
+    AccountName               = "prod-app-account"
+    ManagedOrganizationalUnit = "Prod-Workload"
+    SSOUserEmail              = "diptangshu.chakr@hcl.com"
+    SSOUserFirstName          = "Diptangshu"
+    SSOUserLastName           = "Chakraborty"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+module "sandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "ramkunvar.r@hcl.com"
+    AccountName               = "prod-app2-account"
+    ManagedOrganizationalUnit = "Prod-Workload"
+    SSOUserEmail              = "diptangshu.chakr@hcl.com"
+    SSOUserFirstName          = "Diptangshu"
+    SSOUserLastName           = "Chakraborty"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
